d8be1c34c1fd596a44c5705191c32c08d8b1c656
xp <xp@CETYSDAM00>
2020-01-21 19:00:43 +0100

    ya tiene barra de colores inacabada ,rocio la tiene entera

--- src/codigo/VentanaPaint.java
+++ src/codigo/VentanaPaint.java
@@ -73,7 +73,9 @@ public class VentanaPaint extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        colores1 = new codigo.Colores();
         jPanel1 = new javax.swing.JPanel();
+        colores2 = new codigo.Colores();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -88,11 +90,11 @@ public class VentanaPaint extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 544, Short.MAX_VALUE)
+            .addGap(0, 727, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 288, Short.MAX_VALUE)
+            .addGap(0, 331, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -100,15 +102,21 @@ public class VentanaPaint extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 129, Short.MAX_VALUE)
+                .addGap(0, 110, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(colores2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(34, 34, 34)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(62, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(colores2, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -118,7 +126,7 @@ public class VentanaPaint extends javax.swing.JFrame {
         // aqui dibujamos un punto pa eso necesitamos coordenadas y color 
         //coordinadas proporciona evt
         //aqui el colo hay que pasar con un metodo y podremos dibujar de diferentes colores
-        bufferGraphics.setColor(Color.black);
+        bufferGraphics.setColor(colores2.colorSeleccionado);
         bufferGraphics.fillOval(evt.getX(), evt.getY(), 5, 5);
         repaint(0,0,1,1);
         
@@ -158,8 +166,10 @@ public class VentanaPaint extends javax.swing.JFrame {
             }
         });
     }
-
+    // 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private codigo.Colores colores1;
+    private codigo.Colores colores2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
